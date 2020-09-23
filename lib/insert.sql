1bd1738df24fef715d5a0d5203352db648ef51f1
Jaquan1314 <javaughngrant@gmail.com>
2020-09-22 21:24:06 -0400

    Passed all tests

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 3, "M", "Brown", "Angry", 1);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Rowdy", 6, "M", "Pink", "Friendly", 1);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Tabitha", 2, "F", "Green", "Cheerful", 0);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Sergeant Brown", 5, "M", "Orange", "Stoic", 1);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Melissa", 3, "F", "Brown", "Cheerful", 0);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Grinch", 2, "M", "Grey", "Angry", 0);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Wendy", 8, "F", "Black", "Friendly", 1);
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    (NULL, 5, "F", "Blue", "Angry", 1);
+
